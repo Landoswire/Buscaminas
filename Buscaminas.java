bd053f3a6a789d3d5e4423c0481dc34350d2c0de
Landoswire <61020744+Landoswire@users.noreply.github.com>
2020-10-11 01:26:35 -0500

    Add files via upload

---
+++ Buscaminas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+
+/**
+ *
+ * @author Issei
+ */
+public class Buscaminas {
+    
+    public static void main(String[] args){
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setVisible(true);
+    }
+    
+}
